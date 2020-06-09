80d52a93c3eedccb8ee9e189e5ea5500f89b1da2
faramo_zayw <faramo.zayw@gmail.com>
2020-06-09 22:36:35 +0000

    Updated

--- src/layouts/Head.js
+++ src/layouts/Head.js
@@ -6,12 +6,12 @@ import { menu } from "./../data/navbar.json";
 
 import { findTitleByPath } from "./../components"; 
 
-const Head = ({ location }) => {
-	const subtitle = findTitleByPath(menu)(location);
+const Head = ({ path }) => {
+	const subtitle = findTitleByPath(menu)(path);
 	
 	return (
 		<Helmet>
-			<title>Київський авіаційний технікум | {location} </title>
+			<title>Київський авіаційний технікум | {subtitle} </title>
 			<meta charset="utf-8" />
 			<meta name="description" content="Київський авіаційний технікум" />
 			<meta name="keywords" content="Київський авіаційний технікум, Київський авіаційний технікум сайт, КАТ, КіАТ" />
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -12,7 +12,7 @@ import Head from "./Head";
 
 const Layout = ({ children, location }) => (
 	<div>
-		<Head location={location} />
+		<Head path={location} />
 		<Hero isFullHeight isColor="light">
 			<HeroHeader>
 				<AppHeader />
