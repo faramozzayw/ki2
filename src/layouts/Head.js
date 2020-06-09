7e23b9143893f85df4e5c47667b2395e80373e3f
faramo_zayw <faramo.zayw@gmail.com>
2020-06-09 21:37:29 +0000

    Updated

---
+++ src/layouts/Head.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Helmet } from "react-helmet";
+
+const Head = () => (
+	<Helmet>
+		<title>Київський авіаційний технікум</title>
+		<meta charset="utf-8" />
+		<meta name="description" content="Київський авіаційний технікум" />
+		<meta name="keywords" content="Київський авіаційний технікум, Київський авіаційний технікум сайт, КАТ, КіАТ" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	</Helmet>
+)
+
+export default Head;
