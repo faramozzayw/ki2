bfbf81b4d11f54ece07807aa46ad163a9248c4af
faramo_zayw <faramo.zayw@gmail.com>
2020-06-09 21:55:02 +0000

    Updated

--- src/layouts/Head.js
+++ src/layouts/Head.js
@@ -2,14 +2,19 @@ import React from "react";
 
 import { Helmet } from "react-helmet";
 
-const Head = () => (
-	<Helmet>
-		<title>Київський авіаційний технікум</title>
-		<meta charset="utf-8" />
-		<meta name="description" content="Київський авіаційний технікум" />
-		<meta name="keywords" content="Київський авіаційний технікум, Київський авіаційний технікум сайт, КАТ, КіАТ" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	</Helmet>
-)
+import { menu } from "./../data/navbar.json";
+
+const Head = ({ location }) => {
+
+	return (
+		<Helmet>
+			<title>Київський авіаційний технікум | {location} </title>
+			<meta charset="utf-8" />
+			<meta name="description" content="Київський авіаційний технікум" />
+			<meta name="keywords" content="Київський авіаційний технікум, Київський авіаційний технікум сайт, КАТ, КіАТ" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		</Helmet>
+	)
+}
 
 export default Head;
