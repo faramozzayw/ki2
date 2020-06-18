2676ac3ff5fb8bcdeac2db0d440edb3a24c38b46
faramo_zayw <faramo.zayw@gmail.com>
2020-06-18 12:14:31 +0000

    __test__

--- src/components/FeedbackForm/utils.js
+++ src/components/FeedbackForm/utils.js
@@ -3,12 +3,16 @@ import { setDescription } from "./store/action";
 export const maxTitleLen = 100;
 
 export const createIssue = ({ title, description, type }) => {
-	const repo = "ki2test";
+	const repo = "ki2";
 	const owner = "faramozzayw";
 
 	const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
 
 	const token = process.env.REACT_APP_AUTH_TOKEN;
+
+	fetch(process.env.REACT_APP_AUTH_TOKEN)
+		.then(r => r.json())
+		.then(console.log);
 
 	return fetch(url, {
 		method: "POST",
